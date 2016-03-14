cd9b64c39ee38a891aeae86aaa8a2e38896908f3
Marinus Visser <marinus.c.visser@gmail.com>
2016-03-14 17:01:51 +0100

    remove c# and replace it with f# gui

---
+++ GUI/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+open System.Drawing
+open System.Windows.Forms
+
+let button = 
+    let temp = new Button()
+    temp.Text <- "test"
+    temp.Location <- new Drawing.Point(50, 50)
+    temp
+
+let form = 
+    let temp = new Form()
+    do temp.ClientSize <- new System.Drawing.Size(100,100)
+    temp
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    do Application.Run(form)
+    0 // return an integer exit code
