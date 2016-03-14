27aa3e76effcd2c618852a480d6c88bf93ed5f49
Bart de Ruijter <bederuijter@gmail.com>
2016-03-14 18:01:03 +0100

    Added files
    Quits

--- Logica/Endpoint.fs
+++ Logica/Endpoint.fs
@@ -1,2 +1,15 @@
 ﻿module Endpoint
 
+type Endpoint = 
+    {
+        coordinatesX : int
+        coordinatesY : int
+    }
+    with
+     static member Create(x, y) = 
+        {
+            coordinatesX = x
+            coordinatesY = y
+        }
+
+     override this.ToString() = "Endpoint is located at ( "+this.coordinatesX.ToString()+", "+this.coordinatesY.ToString()+")"
