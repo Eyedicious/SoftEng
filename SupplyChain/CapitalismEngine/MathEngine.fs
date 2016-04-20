e9c82f4a502ab2faad4d539fa8a733835c93fc81
Marinus Visser <marinus.c.visser@gmail.com>
2016-04-20 14:35:26 +0200

    Trucks are now mobile... or at least should be

--- SupplyChain/CapitalismEngine/MathEngine.fs
+++ SupplyChain/CapitalismEngine/MathEngine.fs
@@ -50,16 +50,13 @@ let rec filter (fn:'a -> bool) (e:'a List) =
       else
          filter fn xs
 
+let rec foreachDo (fn:'a->'b) (l:List<'a>) : 'b List =     //Foreach x DO fn y x and return the editted list
+   match l with
+      | [] -> []
+      | (x::xs) ->
+         (fn x) :: (foreachDo fn xs)
 
-let foreachDo (fn:'a->'b->'c) (y:'a) (x:List<'b>) =     //Foreach x DO fn y x and return the editted list
-   let rec inspectX (x:List<'b>) = 
-      match x with
-         | [] -> []
-         | ([x]) -> fn y x :: []
-         | (x::xs) -> (fn y x)::(inspectX xs)
-   inspectX
-
-let updateFactory (dt:float32) (factory:Entities.Factory) =
+let updateFactory (dt:float32) (factory:Entities.Factory) : Factory =
    match factory.laboring with
    |  Entities.Production.Ready ->
       { factory with laboring = Entities.Production.Working 10.0f }
@@ -71,37 +68,69 @@ let updateFactory (dt:float32) (factory:Entities.Factory) =
 
 let UpdateFactories (dt : float32)(spawnNewFactory)(listOfFactories:List<Entities.Factory>) =
    
-   let factories = listOfFactories |> foreachDo updateFactory dt
-   
+   let factories = listOfFactories |> foreachDo (updateFactory dt)
+
    let newFactoriesList =
       if listOfFactories.Length < factoryAmount then
          if spawnNewFactory then
-            EntitiesManager.spawnFactory() :: listOfFactories
+            EntitiesManager.spawnFactory() :: factories
          else
             listOfFactories
       else
          listOfFactories
    newFactoriesList
 
-let UpdateTruck (dt:float32) (truck:Entities.Truck) = 
+let UpdateTruck (dt:float32) (truck:Entities.Truck) =
+   let calcTime = int dt
+   let xCoefficient = truck.route.lastWaypoint().coordinatesX - truck.coordinatesX
+   let yCoefficient = truck.route.lastWaypoint().coordinatesY - truck.coordinatesY
+   let truck = 
+      if truck.route.lastWaypoint().coordinatesX <> truck.coordinatesX then
+         if truck.route.lastWaypoint().coordinatesY <> truck.coordinatesY then
+            if xCoefficient < 5 && xCoefficient > 5 then
+               { truck with coordinatesX = truck.route.lastWaypoint().coordinatesX}
+            else
+               { truck with coordinatesX = (truck.coordinatesX * calcTime * 5)}
+         else
+            if xCoefficient < 10 && xCoefficient > 10 then
+               { truck with coordinatesX = truck.route.lastWaypoint().coordinatesX}
+            else
+               { truck with coordinatesX = int(truck.coordinatesX * calcTime * 10)}
+      else
+         truck
+   let truck = 
+      if truck.route.lastWaypoint().coordinatesY <> truck.coordinatesY then
+         if truck.route.lastWaypoint().coordinatesX <> truck.coordinatesX then
+            if yCoefficient < 5 && yCoefficient > 5 then
+               { truck with coordinatesY = truck.route.lastWaypoint().coordinatesY}
+            else
+               { truck with coordinatesY = (truck.coordinatesY * calcTime * 5)}
+         else
+            if yCoefficient < 10 && yCoefficient > 10 then
+               { truck with coordinatesY = truck.route.lastWaypoint().coordinatesY}
+            else
+               { truck with coordinatesY = (truck.coordinatesY * calcTime * 10)}
+      else
+         truck
    truck
 
 let deployTruck destinations (factory:Factory) = 
    factory.SpawnResource(destinations)
 
-let UpdateTrucks(dt : float32)(factories: List<Entities.Factory>)(trucks : List<Entities.Truck>) : Truck List= 
-   let trucks = trucks |> foreachDo UpdateTruck dt
+let UpdateTrucks(dt : float32)(factories: List<Entities.Factory>)(trucks : List<Entities.Truck>) : Truck List = 
+   let jobWellDone t = 
+      if t.route.lastWaypoint().coordinatesX = t.coordinatesX && t.route.lastWaypoint().coordinatesY = t.coordinatesY then
+         false
+      else true
+   let currentTrucks = filter jobWellDone trucks
+   let currentTrucks = trucks |> foreachDo (UpdateTruck dt)
 
    let isFactoryProductive (f:Entities.Factory) = 
       f.laboring = Production.Ready
    let productiveFactories = filter isFactoryProductive factories
-   let nextShift = foreachDo deployTruck factories factories
-   let jobWellDone (t:Truck) = 
-      if t.route.lastWaypoint().coordinatesX = t.coordinatesX && t.route.lastWaypoint().coordinatesY = t.coordinatesY then
-         false
-      else true
+   let nextShift = foreachDo (deployTruck factories) factories
    
-   trucks @ nextShift
+   currentTrucks @ nextShift
 
 let Update(dt : float32)(gameState : GameState) =
    let spawnNewFactory, newSpawnState =
